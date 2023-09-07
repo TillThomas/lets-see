b9fe83b32f7c80e2a206b5c4596faf499606de81
Till <Till.Thomas.tt@gmail.com>
2023-09-07 09:20:29 +0200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='w-screen h-screen bg-white'><ParentSize>{({ width, height }) => <Map width={width} height={height}  />}</ParentSize></div>
+    <main className="flex w-screen h-screen flex-col items-center justify-center">
+      <div className='w-5/6 h-5/6 bg-white'><ParentSize>{({ width, height }) => <Map width={width} height={height}  />}</ParentSize></div>
     </main>
   )
 }
